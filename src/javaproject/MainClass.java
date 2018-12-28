1fdf37e0222cd1ddf703e176e0b183e940fef880
eogh234 <eogh234@gmail.com>
2018-12-28 19:21:46 +0900

    충돌 해결

--- src/javaproject/MainClass.java
+++ src/javaproject/MainClass.java
@@ -5,13 +5,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("炎");
-<<<<<<< HEAD
 		System.out.println("dsfasdf");
-=======
 		
 		System.out.println("ささささ");
-
->>>>>>> develop
 	}
 
 }
